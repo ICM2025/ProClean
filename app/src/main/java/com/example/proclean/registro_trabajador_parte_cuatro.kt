7b23f12f45fa2f7d1905cfca59d41ac4425c080f
juanealop <88118954+juanealop@users.noreply.github.com>
2025-03-17 23:00:24 -0500

    Primera version de proclean

---
+++ app/src/main/java/com/example/proclean/registro_trabajador_parte_cuatro.kt
@@ -0,0 +1,20 @@
+package com.example.proclean
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class registro_trabajador_parte_cuatro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registro_trabajador_parte_cuatro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
