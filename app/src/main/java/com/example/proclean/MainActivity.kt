283d720341464036dfc9f2dbbecd454d87476d6c
juanealop <88118954+juanealop@users.noreply.github.com>
2025-03-18 16:19:11 -0500

    cambio nuevo

--- app/src/main/java/com/example/proclean/MainActivity.kt
+++ app/src/main/java/com/example/proclean/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proclean.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
     }
